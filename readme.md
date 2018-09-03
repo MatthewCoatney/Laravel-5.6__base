6cbf45cd009e3d83f578d4f76075bdebbf0073d2
MatthewCoatney <matthew@anewcreation.org>
2018-09-03 09:49:42 -0700

    Second commit

---
+++ readme.md
@@ -0,0 +1,6 @@
+<p align="center"><img src="https://laravel.com/assets/img/components/logo-laravel.svg"></p>
+
+
+## Laravel 5.6 - CSS Grid
+
+This is my base Laravel project using CSS Grid. The CSS architecture follows a 7:1 model for scalable apps.
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel 5.6</title>
+
+        <!-- Styles -->
+          <link rel="stylesheet" href="css/main.css">
+        <style>
+
+        </style>
+    </head>
+    <body>
+
+<section class="main">
+    <div class="content">
+        <div class="content__title m-b-md">
+            Laravel
+        </div>
+
+        <div class="content__links">
+            <a href="https://laravel.com/docs">Documentation</a>
+            <a href="https://laracasts.com">Laracasts</a>
+            <a href="https://laravel-news.com">News</a>
+            <a href="https://forge.laravel.com">Forge</a>
+            <a href="https://github.com/laravel/laravel">GitHub</a>
+        </div>
+    </div>
+</section>
+
+
+    </body>
+</html>
